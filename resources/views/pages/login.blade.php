895e613c026e9ad699608142a91f79d2f85105c1
Regyta Harpan <56112090+ronyyanuar@users.noreply.github.com>
2023-07-03 10:34:22 +0700

    refactor

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -49,8 +49,8 @@
                 >
                     <img
                         class="w-6"
-                        alt=""
                         src="{{ Vite::asset('resources/images/logo.svg') }}"
+                        alt=""
                     >
                     <span class="ml-3 text-lg text-white">
                         {{ env('APP_NAME') }}
@@ -59,8 +59,8 @@
                 <div class="my-auto">
                     <img
                         class="-intro-x -mt-16 w-1/2"
-                        alt=""
                         src="{{ Vite::asset('resources/images/illustration.svg') }}"
+                        alt=""
                     >
                     <div class="-intro-x mt-10 text-4xl font-medium leading-tight text-white">
                         {{ __('A few more clicks to') }}
@@ -91,85 +91,90 @@
                         <div class="intro-x mt-8">
                             <input
                                 class="intro-x login__input form-control {{ session()->has('notif') ? 'border-danger' : '' }} block py-3 px-4"
-                                type="text"
                                 name="username"
-                                placeholder="Username"
                                 data-username=""
+                                type="text"
+                                placeholder="Username"
                             >
-                            <input
-                                class="intro-x login__input form-control {{ session()->has('notif') ? 'border-danger' : '' }} mt-4 block py-3 px-4"
-                                type="password"
-                                name="password"
-                                placeholder="{{ __('Password') }}"
-                                data-password=""
-                            >
+                            <div class="relative">
+                                <input
+                                    class="intro-x login__input form-control {{ session()->has('notif') ? 'border-danger' : '' }} mt-4 block py-3 pl-4 pr-10"
+                                    name="password"
+                                    data-password=""
+                                    type="password"
+                                    placeholder="{{ __('Password') }}"
+                                >
+                                <button
+                                    class="absolute top-1/2 right-4 z-50 ml-auto -translate-y-1/2"
+                                    data-password-trigger=""
+                                    type="button"
+                                >
+                                    <div data-show-password>
+                                        <div class="flex items-center gap-1">
+                                            <div>
+                                                <svg
+                                                    class="h-6 w-6"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    stroke-width="1.5"
+                                                    stroke="currentColor"
+                                                >
+                                                    <path
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                                                    />
+                                                    <path
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                                    />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div
+                                        class="hidden"
+                                        data-hide-password
+                                    >
+                                        <div class="flex items-center gap-1">
+                                            <div>
+                                                <svg
+                                                    class="h-6 w-6"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    stroke-width="1.5"
+                                                    stroke="currentColor"
+                                                >
+                                                    <path
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                        d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                                                    />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </button>
+                            </div>
                             @if (session()->has('notif'))
                                 <div class="login__input-error mt-2 text-danger">
                                     {{ session()->get('notif') }}
                                 </div>
                             @endif
                         </div>
-                        <div class="intro-x mt-4 flex text-xs text-slate-600 dark:text-slate-500 sm:text-sm">
-                            <button
-                                class="ml-auto"
-                                type="button"
-                                data-password-trigger=""
-                            >
-                                <div data-show-password>
-                                    <div class="flex items-center gap-1">
-                                        <span>
-                                            {{ __('Show Password') }}
-                                        </span>
-                                        <div>
-                                            <svg
-                                                class="h-6 w-6"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                stroke-width="1.5"
-                                                stroke="currentColor"
-                                            >
-                                                <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                                                />
-                                                <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                                />
-                                            </svg>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div
-                                    class="hidden"
-                                    data-hide-password
-                                >
-                                    <div class="flex items-center gap-1">
-                                        <span>
-                                            {{ __('Hide Password') }}
-                                        </span>
-                                        <div>
-                                            <svg
-                                                class="h-6 w-6"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                stroke-width="1.5"
-                                                stroke="currentColor"
-                                            >
-                                                <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                                                />
-                                            </svg>
-                                        </div>
-                                    </div>
-                                </div>
-                            </button>
+                        <div class="intro-x mt-5 text-center xl:mt-8 xl:text-center">
+                            <p>
+                                <span>
+                                    Login menggunakan
+                                </span>
+                                <a
+                                    class="font-semibold text-blue-700"
+                                    href="{{ route('admin.login.face.index') }}"
+                                >wajah</a>
+                            </p>
                         </div>
                         <div class="intro-x mt-5 text-center xl:mt-8 xl:text-left">
                             <button class="btn btn-primary w-full py-3 px-4 align-top">
